8b283f68a5abca0665b4d10fdb9095a3aa07d601
Benjixx99 <Benjixx99@users.noreply.github.com>
2024-01-25 23:02:46 +0100

    style: Removed a typo

--- Headerfiles/Common/Paths.hpp
+++ Headerfiles/Common/Paths.hpp
@@ -4,12 +4,12 @@
 
 namespace bx {
 	namespace Paths {
-		const std::string assetsConfigFiles = "Ressources/ConfigFiles/Assets";
-		const std::string menuConfigFiles = "Ressources/ConfigFiles/Menu";
-		const std::string uniformConfigFiles = "Ressources/Configfiles/Uniform";
-		const std::string cofingFiles = "Ressources/Configfiles";
-		const std::string databases = "Ressources/Databases";
-		const std::string assets = "Ressources/Assets";
-		const std::string icons = "Ressources/Icons";
+		const std::string assetsConfigFiles = "Resources/ConfigFiles/Assets";
+		const std::string menuConfigFiles = "Resources/ConfigFiles/Menu";
+		const std::string uniformConfigFiles = "Resources/Configfiles/Uniform";
+		const std::string cofingFiles = "Resources/Configfiles";
+		const std::string databases = "Resources/Databases";
+		const std::string assets = "Resources/Assets";
+		const std::string icons = "Resources/Icons";
 	}
 }
