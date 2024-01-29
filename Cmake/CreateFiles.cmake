c5265662fcd2122a51f58ea43c5c06c82ef51300
Benjixx99 <Benjixx99@users.noreply.github.com>
2024-01-29 21:58:10 +0100

    Added some textures and config files

--- Cmake/CreateFiles.cmake
+++ Cmake/CreateFiles.cmake
@@ -1,19 +1,4 @@
 
-set(ANIMATION_CONFIG_FILE "${RESOURCES_CONFIGFILES}/Assets/animationsConfig.txt")
-if(NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${ANIMATION_CONFIG_FILE}")
-    file(WRITE "${CMAKE_CURRENT_SOURCE_DIR}/${ANIMATION_CONFIG_FILE}"
-    "String  TextureName	 PlayerMovingExample\n"
-    "Int     NumberOfFrames  2\n"
-    "Int     AnimationSpeed  30\n"
-    "End\n\n"
-        
-    "String  TextureName 	 EnemyMovingExample\n"
-    "Int     NumberOfFrames  4\n"
-    "Int     AnimationSpeed  20\n"
-    "End\n\n"
-    )
-endif()
-
 set(EXAMPLE_UNIFORM_FILE "${RESOURCES_CONFIGFILES}/Uniform/ExampleFileOfUniformForShaders.txt")
 if(NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${EXAMPLE_UNIFORM_FILE}")
     file(WRITE "${CMAKE_CURRENT_SOURCE_DIR}/${EXAMPLE_UNIFORM_FILE}"
